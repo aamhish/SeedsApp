a07d761467632e685257d3dcab0886bd79e9467a
Aamhish <aamhish@gmail.com>
2024-01-20 17:28:49 -0500

    Initial Commit

---
+++ SeedsForReal/SeedsForRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeedsForRealApp.swift
+//  SeedsForReal
+//
+//  Created by Aamhish Rao on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SeedsForRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
